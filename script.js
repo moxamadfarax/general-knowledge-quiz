276ca5c7cf32311a0bbd7b639742c152dd4fce6c
Mohamed_Farah <mohamedfarah0508@gmail.com>
2022-11-17 05:15:38 -0800

    second commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var navbar = document.getElementById('#nav')
+var startBtn =document.getElementById('#start-button')
+var questions = document.getElementById('#questions')
+
+startBtn.addEventListener('click', hideStart)
+
