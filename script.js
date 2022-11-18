be87af055ef5537c9774abf22ad83373c3e16b31
Mohamed Farah <mohamedfarah0508@gmail.com>
2022-11-17 16:52:07 -0800

    finishd function

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-var navbar = document.getElementById('#nav')
-var startBtn =document.getElementById('#start-button')
-var questions = document.getElementById('#questions')
+var startBtn = document.getElementById('start-btn')
+var startPage = document.getElementById('start-page')
+var questionsPage = document.getElementById('questions')
+questionsPage.classList = 'hidden'
 
-startBtn.addEventListener('click', hideStart)
+
+startBtn.addEventListener('click', function hideStart(){
+questionsPage.classList = 'show'
+startPage.classList = 'hidden'
+    var timeleft = 60;
+    var countDown = document.getElementById('timer')
+var minuteTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById('timer').innerHTML = "Finished";
+  } else {
+    document.getElementById('timer').innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
+
+})
+
+var countDown = document.getElementById('timer')
+
+
+
+
+
 
